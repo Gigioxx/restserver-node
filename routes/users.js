e21eb75d23faa2246377f4546e0aea1e51324b77
Gigiox <guillermo.casanova.b@gmail.com>
2022-02-21 23:57:44 -0300

    Validating required fields

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
+const { fieldsValidation } = require('../middlewares/fields-validation');
+
 const { getUsers, putUsers, postUsers, deleteUsers, patchUsers } = require('../controllers/users');
 
 const router = Router();
@@ -11,7 +13,11 @@ router.get('/', getUsers );
 router.put('/:id', putUsers );
 
 router.post('/', [
+    check('name', 'Name must have a value').not().isEmpty(),
+    check('password', 'Password must have a value and be greater than 6 characters').isLength({ min: 6 }),
     check('email', 'Email is not valid').isEmail(),
+    check('role', 'Is not a valid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    fieldsValidation
 ], postUsers );
 
 router.delete('/', deleteUsers );
