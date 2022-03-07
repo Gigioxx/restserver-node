a9a8ac3dc24711c48e5283a5b0a3d80f46ffd18e
Gigiox <guillermo.casanova.b@gmail.com>
2022-03-07 12:57:48 -0300

    Uploading files using express-fileupload

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,9 +1,23 @@
+const path = require('path');
+
 const { response } = require('express');
 
 const loadFile = ( req, res = response ) => {
 
-    res.json({
-        msg: 'Hello world'
+    if ( !req.files || Object.keys(req.files).length === 0 || !req.files.file ) {
+        return res.status(400).json({ msg: 'No files were uploaded.' });
+    }
+    
+    const { file } = req.files;
+    const uploadPath = path.join( __dirname, '../uploads/', file.name );
+
+    // Use the mv() method to place the file somewhere on your server
+    file.mv(uploadPath, (err) => {
+        if (err) {
+            return res.status(500).json({ err });
+        }
+
+        res.json({ msg: 'File uploaded to ' + uploadPath });
     });
 
 }
