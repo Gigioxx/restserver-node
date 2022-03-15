0b4a6cf583550d3e745291c96f28af2b1e0f9db6
Gigiox <guillermo.casanova.b@gmail.com>
2022-03-15 17:25:19 -0300

    Validating JWT frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,37 @@
+const url = ( window.location.hostname.includes('localhost') )
+            ? 'http://localhost:8080/api/auth/'
+            : 'https://nodejs-restsv.herokuapp.com/api/auth';
 
+let user = null;
+let socket = null;
 
-const socket = io();
+// Validating localstorage token
+
+const validateJWT = async() => {
+
+    const token = localStorage.getItem('token') || '';
+
+    if ( token.length <= 10 ) {
+        window.location = 'index.html';
+        throw new Error('There is no token on server');
+    }
+
+    const resp = await fetch( url, { 
+        headers: { 'x-token': token }
+    });
+
+    const { user: userDB, token: tokenDB } = await resp.json();
+    localStorage.setItem( 'token', tokenDB );
+
+}
+
+const main = async() => {
+
+    // Validating JWT
+    await validateJWT();
+
+}
+
+main();
+
+// const socket = io();
